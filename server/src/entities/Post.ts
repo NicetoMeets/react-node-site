2481dc80783587ea7895537e39f9809decacb76d
Nice <puma1800@naver.com>
2022-11-22 23:47:59 +0900

    헬퍼스

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,3 +1,13 @@
+import { Exclude, Expose } from 'class-transformer';
+import { BeforeInsert, Column, Entity, Index, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+import { makeId, slugify } from '../utils/helpers';
+import Comment from './Comment';
+import BaseEntity from './Entity';
+import Sub from './Sub';
+import { User } from './User';
+import Vote from './Vote';
+
+
 @Entity("posts")
 export default class Post extends BaseEntity {
     @Index()
