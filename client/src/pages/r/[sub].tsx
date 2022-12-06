f77ac1307a3de43c4fffe3c48d053d23e538deea
Nice <puma1800@naver.com>
2022-12-07 00:41:36 +0900

    커뮤니티 상세페이지

---
+++ client/src/pages/r/[sub].tsx
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import React from 'react';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+
+const SubPage = () => {
+
+    const fetcher = async (url: string) => {
+        try {
+            const res = await axios.get(url);
+            return res.data;
+        } catch (error: any) {
+            throw error.response.data;
+        }
+    }
+
+    const router = useRouter();
+    const subName = router.query.sub;
+    const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : null, fetcher);
+    console.log('sub', sub)
+    return (
+        <>
+            {sub &&
+                <>
+                    <div>
+                        <input/>
+                        {/* 배너 이미지 */}
+                        <div className="bg-gray-400">
+                            {sub.bannerUrl ? (
+                                <div
+                                    className='h-56'
+                                    style={{
+                                        backgroundImage: `url(${sub.bannerUrl})`,
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundSize: 'cover',
+                                        backgroundPosition: 'center',
+                                    }}
+                                    
+                                >
+                                </div>
+                            ) : (
+                                <div className='h-20 bg-gray-400'
+                                    
+                                ></div>
+                            )}
+                        </div>
+                        {/* 커뮤니티 메타 데이터 */}
+                        <div className='h-20 bg-white'>
+                            <div className='relative flex max-w-5xl px-5 mx-auto'>
+                                <div className='absolute' style={{ top: -15 }}>
+                                    {sub.imageUrl && (
+                                        <Image
+                                            src={sub.imageUrl}
+                                            alt="커뮤니티 이미지"
+                                            width={70}
+                                            height={70}
+                                            className="rounded-full"
+                                            
+                                        />
+                                    )}
+                                </div>
+                                <div className='pt-1 pl-24'>
+                                    <div className='flex items-center'>
+                                        <h1 className='text-3xl font-bold '>{sub.title}</h1>
+                                    </div>
+                                    <p className='font-bold text-gray-400 text-small'>
+                                        /r/{sub.name}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </>
+            }
+        </>
+    )
+}
+
+export default SubPage
